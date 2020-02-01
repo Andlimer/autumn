8225f7d55d1b09db80aa6f02487bd546171c9f62
Evgeny Gurtovoy <Andlimer@gmail.com>
2020-02-01 15:12:17 +1000

    Сделал слайдер отзывов

---
+++ src/scripts/reviews.js
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+
+new Vue ({
+  el: "#reviews-component",
+  template: "#reviews",
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {
+    return {
+      reviews: [],
+      swiperOption: {
+        navigation: {
+          nextEl: ".reviews__btn_next",
+          prevEl: ".reviews__btn_prev"
+        },
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 84
+      }
+    }
+  },
+  methods: {
+    arrWithRequiredImage(data) {
+      return data.map(item => {
+        const requireImg = require(`../images/content/${item.photo}`);
+        item.photo = requireImg;
+        return item;
+      });
+    }
+  },
+  created() {
+    const data = require("../data/reviews.json");
+    this.reviews = this.arrWithRequiredImage(data);
+  }
+});
